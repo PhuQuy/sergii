35617792a50c16e52d0ea8c44fda2a3f8558c35e
PhuQuy <phuquy.tool@gmail.com>
2018-07-23 10:03:09 +0700

    update first page

--- src/app/routes/your-quote/your-quote.component.ts
+++ src/app/routes/your-quote/your-quote.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YourQuoteComponent implements OnInit {
     confirm: boolean = false;
+    sex:string = 'gender';
     constructor() { }
 
     ngOnInit() {
