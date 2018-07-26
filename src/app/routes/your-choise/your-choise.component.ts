91834c45e3f3292c0989062de44df9445d4ff8b9
PhuQuy <phuquy.uit@gmail.com>
2018-07-26 10:23:38 +0700

    progress bar

--- src/app/routes/your-choise/your-choise.component.ts
+++ src/app/routes/your-choise/your-choise.component.ts
@@ -12,18 +12,25 @@ export class YourChoiseComponent implements OnInit {
     };
 
     slide = 1;
+    percent = 0;
     constructor(public router: Router) { }
 
     ngOnInit() {
+        this.percent = this.slide * 100 / 7;
     }
 
     nextSlide() {
         this.slide++;
+        this.percent = this.slide * 100 / 7;
+
+
     }
 
     previousSlide() {
         if (this.slide > 1) {
             this.slide--;
+            this.percent = this.slide * 100 / 7;
+
         }
     }
 
