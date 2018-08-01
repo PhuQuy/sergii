d78e3fde865a99918f613a6b171d15c4143f4ca0
PhuQuy <phuquy.tool@gmail.com>
2018-08-01 16:18:20 +0700

    update port

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-  port: process.env.PORT || 80,
+  port: process.env.PORT || 8080,
   db: process.env.MONGODB_URI || 'mongodb://phuquy:XXxx11!!@phuquyitachi-shard-00-00-sj4th.mongodb.net:27017,phuquyitachi-shard-00-01-sj4th.mongodb.net:27017,phuquyitachi-shard-00-02-sj4th.mongodb.net:27017/test?ssl=true&replicaSet=phuquyitachi-shard-0&authSource=admin&retryWrites=true',
   emailSerive: process.env.EMAIL_SERVICE || 'gmail',
   emailHost: process.env.EMAIL_HOST || 'localhost',
