09ce5aa07488e2861636f885843a01e20917a86b
PhuQuy <nvkhanh1775@gmail.com>
2018-08-23 19:32:22 +0700

    update

--- server/api/modules/order/order.routes.js
+++
@@ -1,9 +0,0 @@
-// import { Router } from 'express';
-// import * as orderHandler from './order.handler';
-
-// export function init(api){
-//   const router = new Router();
-//   router.get('/', orderHandler.getAllOrders);
-
-//   api.use('/order', router);
-// }
