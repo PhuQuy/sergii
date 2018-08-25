20de0091f83ea33ea4d7d4f049544b5f918ec0ad
PhuQuy <phuquy.tool@gmail.com>
2018-08-25 15:53:12 +0700

    update

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -55,6 +55,7 @@ const routes: Routes = [
     {
         path: 'admin',
         component: AdminComponent,
+        canActivate: [AuthGuard],
         children: [
             {
                 path: 'users',
