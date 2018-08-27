7d9bc32ce2d4c59ac7b859744ba02177b66487a1
PhuQuy <phuquy.tool@gmail.com>
2018-08-27 13:44:39 +0700

    update

--- src/app/routes/admin/admin.component.ts
+++ src/app/routes/admin/admin.component.ts
@@ -1,19 +1,34 @@
 import {
-  Component,
-  OnInit,
+    Component,
+    OnInit,
+    ElementRef
 } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.states';
+import { LogOut } from '../../store/actions/auth.actions';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: [ './admin.component.css' ],
+    selector: 'app-admin',
+    templateUrl: './admin.component.html',
+    styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
+    private toggleButton: any;
+    private sidebarVisible: boolean;
+    user: string;
+    constructor(private store: Store<AppState>, private router: Router, private userService: UserService) {
+        this.sidebarVisible = false;
+    }
 
-  constructor() {
-  }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
+    logout() {
+        this.store.dispatch(new LogOut);
+        this.userService.logout().subscribe(() => {
 
+        })
+        this.router.navigateByUrl('/');
+    }
 }
